f340755f5c98c1b53da29e1fdb42706d008792ab
Nick Myers <zenkitties@comcast.net>
2015-04-07 10:15:36 -0700

    added in some fixes to the mobile get estimate button

--- wp-content/themes/redwavecommunications/index.php
+++ wp-content/themes/redwavecommunications/index.php
@@ -1,35 +1,36 @@
 <?php get_header(); ?>
 		<!-- section -->
-		<?php $query = rw_homepage_residential_links(); ?>
+				<?php $query = rw_homepage_residential_links(); ?>
 
 			<!--==================== main content start =======================-->
 
-			<div class="col-md-24">
-				<div class="col-md-12" id="left">
-					<h4 class="red">residential<br />solutions</h4>
+			<div class="col-md-24" id="mainPage">
+					<div class="col-md-12" id="left">
+						<h4 class="red">residential<br />solutions</h4>
 
 
-					<ul>
-						<?php if ($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
-						<li><a href="<?php echo types_render_field('link', array('output' => 'raw')); ?>"><?php the_title(); ?> ></a></li>
-						<?php endwhile; endif; ?>
-					</ul>
-				</div><!-- left side column -->
+						<ul>
+							<?php if ($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
+							<li><a href="<?php echo types_render_field('link', array('output' => 'raw')); ?>"><?php the_title(); ?> ></a></li>
+							<?php endwhile; endif; ?>
+						</ul>
+					</div><!-- left side column -->
 
-		<?php $query = rw_homepage_business_links(); ?>
-				<div class="col-md-12" id="right">
-					<h4 class="red">business<br /> solutions</h4>
+				<?php $query = rw_homepage_business_links(); ?>
+					<div class="col-md-12" id="right">
+						<h4 class="red">business<br /> solutions</h4>
 
-					<ul>
-						<?php if ($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
-						<li><a href="<?php echo types_render_field('link', array('output' => 'raw')); ?>"><?php the_title(); ?> ></a></li>
-						<?php endwhile; endif; ?>
-					</ul>
-				</div><!-- right side column -->
+						<ul>
+							<?php if ($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
+							<li><a href="<?php echo types_render_field('link', array('output' => 'raw')); ?>"><?php the_title(); ?> ></a></li>
+							<?php endwhile; endif; ?>
+						</ul>
+					</div><!-- right side column -->
+					<div id="getestimate">
+						<p><a href="contact-us" target="_self">Get an Estimate</a></p>
+					</div>
 			</div>
-			<div id="getestimate">
-				<p><a href="contact-us" target="_self">Get an Estimate</a></p>
-			</div>
+
 			<!--=================main content end ==========================-->
 		<!-- /section -->
 
