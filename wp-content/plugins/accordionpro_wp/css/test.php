c0202388a60d60b784320306c78ba0b7084d5390
sramos9191 <sramos9191@hotmail.com>
2015-05-01 16:27:59 -0700

    adding changes

---
+++ wp-content/plugins/accordionpro_wp/css/test.php
@@ -0,0 +1,50 @@
+<div class="container-fluid">
+    <div class="row"><img src="/wp-content/uploads/2015/02/residential-overview.png" alt="residential solutions" />
+    </div><!-- image row-->
+
+    <div class="col-xs-24">
+        <p class="content">From surround sound to smart home systems, Red Wave Communications can integrate security and entertainment seamlessly into your everyday life. Through quality products, original design, and 24/7 customer support, we can simplify your home integration.</p><br />
+        &nbsp;
+        <div class="col-xs-4 estimate"><a href="contact-us">Get an estimate</a></div>
+        <!-- get estimate link -->
+
+    </div>
+    <!-- content -->
+    <div class="col-xs-24 vertical-align">
+        <h3><span class="red">SERVICES</span><a href="/services/"><img class="square" src="/wp-content/uploads/2015/02/red-square.png" alt="" /></a></h3>
+        <ul class="bullets">
+            <li>Design</li>
+            <li>Build</li>
+            <li>Maintain</li>
+        </ul>
+    </div>
+    <!-- services -->
+    <div class="col-xs-24 vertical-align">
+        <h3><span class="red square">SYSTEMS</span><a href="#accordionPro214-slide-5"><img class="square" src="/wp-content/uploads/2015/02/red-square.png" alt="" /></a></h3>
+        <ul class="bullets">
+            <li>Audiovisual</li>
+            <li>Central &amp; Cellular Monitoring</li>
+            <li>Fire Alarm</li>
+            <li>Life Safety</li>
+            <li>Remote Access</li>
+            <li>Security Systems &amp; Cameras</li>
+            <li>Sound</li>
+            <li>Video Surveillance</li>
+        </ul>
+    </div>
+    <!-- systems-->
+    <div class="col-xs-24 vertical-align">
+        <h3><span class="red">BRANDS</span></h3>
+        <ul class="bullets">
+            <li>Control4</li>
+            <li>Crestron</li>
+            <li>DMP: Digital Monitoring Products</li>
+            <li>GE</li>
+            <li>SnapAV</li>
+            <li class="noBullets">...and more</li>
+        </ul>
+    </div>
+    <!-- overall row end -->
+
+</div>
+<!-- container fluid -->
