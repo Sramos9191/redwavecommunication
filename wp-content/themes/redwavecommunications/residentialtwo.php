3c2f5405485975377125e6f9f94099ccbd2332a0
sramos9191 <sramos9191@hotmail.com>
2015-05-04 16:53:06 -0700

    remove git conflicts

--- wp-content/themes/redwavecommunications/residentialtwo.php
+++ wp-content/themes/redwavecommunications/residentialtwo.php
@@ -6,11 +6,7 @@
         <?php echo do_shortcode("[accordion_pro id='214']"); ?>
     </div>
 
-<<<<<<< HEAD
-    <div class="visible-xs visible-sm">
-        <?php echo do_shortcode("[accordion_pro id='175']"); ?>
-    </div>
-=======
+
     <!--===================== Business Accordion ==========================-->
 
     <div class="visible-md visible-lg">
@@ -20,9 +16,7 @@
     <div class="visible-xs visible-sm">
         <?php echo do_shortcode("[accordion_pro id='175']"); ?>
     </div>
-
->>>>>>> 1276638d733691cb3bc4b7d62a42ca47fe721106
-
+    
     <?php get_footer(); ?>
 
 </main>
